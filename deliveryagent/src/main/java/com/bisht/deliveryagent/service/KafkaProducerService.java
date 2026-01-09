33c7b19c0d666dedd2e42df31fbd4d6be2d21784
Gaurav Bisht <71581943+Gaurav13Bisht@users.noreply.github.com>
2026-01-10 00:48:58 +0530

    Update KafkaProducerService to use DeliveryUpdateAVRO
    Uncommented KafkaTemplate send methods for DeliveryUpdateAVRO.

--- deliveryagent/src/main/java/com/bisht/deliveryagent/service/KafkaProducerService.java
+++ deliveryagent/src/main/java/com/bisht/deliveryagent/service/KafkaProducerService.java
@@ -13,7 +13,7 @@ public class KafkaProducerService {
 
     @Autowired
 //    private KafkaTemplate<Integer, DeliveryUpdate> kafkaTemplate; // Data types denotes what type of data is used in its functions like send
-    private KafkaTemplate<Integer, DeliveryUpdateAVRO> kafkaTemplate; // Data types denotes what type of data is used in its functions like send
+    private KafkaTemplate<Integer, DeliveryUpdateAVRO> kafkaTemplate;
 
     public boolean updateLocation(String remainingDistance){
         try {
@@ -25,12 +25,21 @@ public class KafkaProducerService {
                             .setStatus("Okay")
                             .setEstimatedTimeInMins("45").build());
 
+            // Can send key as well, all the data with same key gets stored in same partition
+            // Useful for scenarios where multiple data needs to be processed in order since each partition maintains order
+            // kafkaTemplate.send(DeliveryConstants.TOPIC_NAME, "key", DeliveryUpdateAVRO.newBuilder()
+            //                 .setDistanceRemaining(remainingDistance)
+            //                 .setDriverName("Not Gaurav")
+            //                 .setStatus("Okay")
+            //                 .setEstimatedTimeInMins("45").build());
+
 //            kafkaTemplate.send(DeliveryConstants.TOPIC_NAME, DeliveryUpdate.builder().distanceRemaining(remainingDistance)
 //                    .driverName("Not Gaurav")
 //                    .estimatedTimeInMins("45")
 //                    .build());
 //            kafkaTemplate.send(DeliveryConstants.TOPIC_NAME, remainingDistance);
             // We can also mention partition number or key if want to send to a particular partition
+            // Here partition = 0 and key = 12
 //            kafkaTemplate.send(DeliveryConstants.TOPIC_NAME, 0, 12, remainingDistance);
         }
         catch (Exception e){
